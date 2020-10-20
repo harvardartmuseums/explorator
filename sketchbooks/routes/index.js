85c4cf18dd65174d7f681cc4cf0cb6bdfffbdb06
Jeff Steward <steward.jeff@gmail.com>
2020-10-20 10:15:39 -0400

    rework sketchbook demo land page

--- sketchbooks/routes/index.js
+++ sketchbooks/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Sketchbook Demos | Explorator | Harvard Art Museums' });
+});
+
 /* GET most viewed online page. */
-router.get('/piene', function(req, res, next) {
-  res.render('piene-index', {layout: 'layout.hbs', title: 'Otto Piene Sketchbook Explorer | Explorator | Harvard Art Museums' });
+router.get('/piene/colors', function(req, res, next) {
+  res.render('piene-colors', {layout: 'layout.hbs', title: 'Otto Piene Color | Explorer | Sketchbook Demos | Explorator | Harvard Art Museums' });
 });
 
 module.exports = router;
