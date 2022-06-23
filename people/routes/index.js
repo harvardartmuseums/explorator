77bbacb46a88a079bbc6522710afe9b32db5c2d2
Jeff Steward <steward.jeff@gmail.com>
2022-06-23 16:12:23 -0400

    make people explorer more interesting

--- people/routes/index.js
+++ people/routes/index.js
@@ -7,13 +7,15 @@ router.get('/', function(req, res, next) {
   res.render('people-index', {layout: 'layout.hbs', title: 'People Explorer | Explorator | Harvard Art Museums' });
 });
 
-router.get('/:id', function(req, res, next) {
+router.get('/:id', async function(req, res, next) {
   let criteria =  req.params.id;
-  HAM.People.get(criteria)
-     .then(data => {
-        res.render('people-details', {layout: 'layout.hbs', title: 'People Explorer | Explorator | Harvard Art Museums', person: data});
-     });
+  
+  let person = await HAM.People.get(criteria);
+  let objects = await HAM.Objects.search({person: person.id, hasimage: 1, sort: 'random', q: 'imagepermissionlevel:0'});
 
+  person.objects = objects.records;
+
+  res.render('people-details', {layout: 'layout.hbs', title: 'People Explorer | Explorator | Harvard Art Museums', person: person});
 });
 
 module.exports = router;
