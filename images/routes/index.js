657d8d2545ced7e391a7e9b773aeffb395e08b94
Jeff Steward <steward.jeff@gmail.com>
2022-11-08 17:18:30 -0500

    initial version

--- images/routes/index.js
+++ images/routes/index.js
@@ -22,10 +22,49 @@ router.get('/alt-text/roulette', function(req, res, next) {
     size: 1
   }
   HAM.Objects.search(criteria)
-     .then(data => {
-        let record = data.records[0];
-        res.render('roulette', {layout: '../../core/views/layout.hbs', title: 'Alt Text Roulette | Image Explorer | Explorator | Harvard Art Museums', object: record});
-     });
+  .then(data => {
+    let record = data.records[0];
+    res.render('roulette', {layout: '../../core/views/layout.hbs', title: 'Alt Text Roulette | Image Explorer | Explorator | Harvard Art Museums', object: record});
+  });
+});
+
+router.get('/collage', function(req, res, next) {
+  res.render('collage', {layout: '../../core/views/layout.hbs', title: 'Collage It | Image Explorer | Explorator | Harvard Art Museums' });
+});
+
+router.get('/collage/viewer', async function(req, res, next) {
+  let criteria = {
+    sort: 'random', 
+    classification: req.query.types, 
+    fields: 'title,images', 
+    hasimage: 1, 
+    size: req.query.quantity
+  };  
+
+  let results = await HAM.Objects.search(criteria);
+  let records = results.records;
+  let images = [];
+
+  for (let index = 0; index < req.query.quantity; index++) {
+    const object = records[index];
+    
+    let image = object.images[0];
+    image.centerPoint = {
+      x: Math.floor(image['width']/2),
+      y: Math.floor(image['height']/2)
+    };
+    
+    image.collage = [];
+    image.collage.push(`${image['iiifbaseuri']}/0,0,${image.centerPoint.x},${image.centerPoint.y}/!300,300/0/native.jpg`);
+    image.collage.push(`${image['iiifbaseuri']}/${image.centerPoint.x},0,${image.centerPoint.x},${image.centerPoint.y}/!300,300/0/native.jpg`);
+    image.collage.push(`${image['iiifbaseuri']}/${image.centerPoint.x},${image.centerPoint.y},${image.centerPoint.x},${image.centerPoint.y}/!300,300/0/native.jpg`);
+    image.collage.push(`${image['iiifbaseuri']}/0,${image.centerPoint.y},${image.centerPoint.x},${image.centerPoint.y}/!300,300/0/native.jpg`);
+
+    image.object = object;
+    images.push(image);
+  };
+  
+  res.render('collage-viewer', {layout: '../../core/views/layout.hbs', title: 'Collage It | Image Explorer | Explorator | Harvard Art Museums', images: images});
 });
 
 module.exports = router;
