b0e3ad628c6ff851878e2afdf3bf3f528f4f53cc
Jeff Steward <steward.jeff@gmail.com>
2023-05-17 14:48:10 -0400

    added related exhibitions to people pages

--- people/routes/index.js
+++ people/routes/index.js
@@ -11,9 +11,22 @@ router.get('/:id', async function(req, res, next) {
   let criteria =  req.params.id;
   
   let person = await HAM.People.get(criteria);
-  let objects = await HAM.Objects.search({person: person.id, hasimage: 1, sort: 'random', q: 'imagepermissionlevel:0'});
 
+  let objects = await HAM.Objects.search({person: person.id, hasimage: 1, sort: 'random', q: 'imagepermissionlevel:0'});
   person.objects = objects.records;
+  
+  person.exhibitions = [];
+  person.exhibitioncount = 0;
+  
+  let objectsExhibited = await HAM.Objects.search({person: person.id, size:0}, {'exhibitions': {'terms': {'field': 'exhibitions.exhibitionid', 'size': 200}}});
+  let exhibitionIds = objectsExhibited.aggregations.exhibitions.buckets.map(e => e.key).join("|");
+  
+  if (exhibitionIds) {
+    let exhibitions = await HAM.Exhibitions.search({id: exhibitionIds, sort: 'chronological', sortorder: 'desc'});
+
+    person.exhibitioncount = exhibitions.info.totalrecords;
+    person.exhibitions = exhibitions.records;
+  }
 
   res.render('people-details', {layout: '../../core/views/layout.hbs', title: 'People Explorer | Explorator | Harvard Art Museums', person: person});
 });
