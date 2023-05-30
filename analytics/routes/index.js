dc6bb3099fb511c326ee797bd608a4a07da34aa7
Jeff Steward <steward.jeff@gmail.com>
2023-05-30 17:09:10 -0400

    overhauled the analytics section

--- analytics/routes/index.js
+++ analytics/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', function(req, res, next) {
+  res.render('index', {layout: '../../core/views/layout.hbs', title: 'Analytics Explorer | Explorator | Harvard Art Museums' });
+});
+
+
 /* GET most viewed online page. */
 router.get('/most-viewed', function(req, res, next) {
-  res.render('analytics-most-viewed-online', {layout: 'layout.hbs', title: 'Most Viewed Online | Analytics Explorer | Explorator | Harvard Art Museums' });
+  res.render('analytics-most-viewed-online', {layout: '../../core/views/layout.hbs', title: 'Most Viewed Online | Analytics Explorer | Explorator | Harvard Art Museums' });
 });
 
 module.exports = router;
