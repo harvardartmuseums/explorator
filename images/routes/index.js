428b896f5d594bc616863db3d9e53a4036d40b56
Jeff Steward <steward.jeff@gmail.com>
2024-01-26 09:29:25 -0500

    add openai descriptions to image browser

--- images/routes/index.js
+++ images/routes/index.js
@@ -1,5 +1,6 @@
 var router = require("express-promise-router")();
 var ham = require('@harvardartmuseums/ham');
+var _ = require('lodash');
 
 let HAM = new ham(process.env.apikey);
 
@@ -11,6 +12,39 @@ router.get('/', function(req, res, next) {
 /* GET the image browser page. */
 router.get('/browse', function(req, res, next) {
   res.render('browse', {layout: '../../core/views/layout.hbs', title: 'Browser | Image Explorer | Explorator | Harvard Art Museums' });
+});
+
+router.get('/browse/data', async function(req, res, next) {
+  let criteria = {
+    q: "images.alttext:* AND imagepermissionlevel:0",
+    classification: "any",
+    sort: "id",
+    page: req.query.page,
+    size: req.query.size
+  };
+
+  let objects = await HAM.Objects.search(criteria);
+
+  let imageIdList = objects.records.map(r => r.images[0].imageid).join("|");
+  criteria = {
+    image: imageIdList,
+    fields: 'body,imageid',
+    q: 'source: "Azure OpenAI Service"'
+  };
+  let annotations = await HAM.Annotations.search(criteria);
+  annotations.records.forEach(a => {
+    let object = _.find(objects.records, {images: [{imageid: a.imageid}]});
+    if (object) {
+      let image = _.find(object.images, {imageid: a.imageid});
+      if (image) {
+        image.openaidescription = a.body;
+      }
+    }
+  })
+
+  objects.info.next = null;
+  objects.info.prev = null;
+  res.json(objects);
 });
 
 /* GET the image alt text roulette page. */
