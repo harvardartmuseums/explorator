cf6fc68c9d4063f864a2a1919ed51bb4e90e2080
Jeff Steward <steward.jeff@gmail.com>
2024-04-10 17:00:39 -0400

    recreate the basement

--- analytics/routes/index.js
+++ analytics/routes/index.js
@@ -1,10 +1,12 @@
-var express = require('express');
-var router = express.Router();
+let router = require("express-promise-router")();
 
 router.get('/', function(req, res, next) {
   res.render('index', {layout: '../../core/views/layout.hbs', title: 'Analytics Explorer | Explorator | Harvard Art Museums' });
 });
 
+router.get('/least-active', async function(req, res, next) {
+  res.render('least-active', {layout: '../../core/views/layout.hbs', title: 'Least Active | Analytics Explorer | Explorator | Harvard Art Museums'})
+});
 
 /* GET most viewed online page. */
 router.get('/most-viewed', function(req, res, next) {
